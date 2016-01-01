ad6b9196698ae63be1cc5e7e55fd82645722dfc7
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2016-01-01 16:25:09 +0700

    handle empty result

--- lib/format_table.ex
+++ lib/format_table.ex
@@ -1,6 +1,10 @@
 defmodule FormatTable do
   @header ["Package Name", "Description", "Version", "Link", "Download Counter"]
   @title "Hex Searcher"
+  def print([]) do
+    IO.puts "There is no results found."
+  end
+  
   def print(packages) do
     to_list = fn(p) ->
       [p.name, p.description, p.version, p.url, p.download ]
@@ -11,4 +15,6 @@ defmodule FormatTable do
     |> TableRex.quick_render!(@header, @title)
     |> IO.puts
   end
+
+  
 end
