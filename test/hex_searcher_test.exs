d26989ca619ee1f72d3cc97a3405b73154679517
nguyen vinh linh <nguyenvinhlinh@localhost.localdomain>
2016-10-03 01:22:36 +0700

    update test

--- test/hex_searcher_test.exs
+++ test/hex_searcher_test.exs
@@ -1,23 +1,11 @@
 defmodule HexSearcherTest do
   use ExUnit.Case
-  @packages  [%HexPackage{description: "Small, fast, modular HTTP server written in Erlang.",
-                          download: "602,410", name: "cowboy", url: "https://hex.pm/packages/cowboy",
-                          version: "1.0.4"},
-              %HexPackage{description: "Support library for manipulating Web protocols.",
-                          download: "596,980", name: "cowlib", url: "https://hex.pm/packages/cowlib",
-                          version: "1.3.0"},
-              %HexPackage{description: "Erlang/Elixir Cloud Framework cowboy HTTP/HTTPS Service",
-                          download: "125", name: "cloudi_service_http_cowboy",
-                          url: "https://hex.pm/packages/cloudi_service_http_cowboy", version: "1.5.1"},
-              %HexPackage{description: "A cow-friend who will speak your mind",
-                          download: "74", name: "cowsay", url: "https://hex.pm/packages/cowsay",
-                          version: "0.0.1"},
-              %HexPackage{description: "ButlerCowsay plugin for Butler SlackBot",
-                          download: "23", name: "butler_cowsay",
-                          url: "https://hex.pm/packages/butler_cowsay", version: "0.2.1"},
-              %HexPackage{description: "Small, fast, modular HTTP server written in Elixir.",
-                          download: "3", name: "cowgirl", url: "https://hex.pm/packages/cowgirl",
-                          version: "0.0.1"}]
+  @packages  [%HexPackage{description: "Fancy HTTP client for Elixir, based on ibrowse.", download: "162,301",
+                          name: "httpotion", url: "https://hex.pmhttps://hex.pm/packages/httpotion", version: "3.0.2"},
+              %HexPackage{description: "milkpotion is an api wrapper for Remember the Milk", download: "130",
+                          name: "milkpotion", url: "https://hex.pmhttps://hex.pm/packages/milkpotion", version: "0.0.4"},
+              %HexPackage{description: "Substitute placeholders in ODT's with custom information", download: "79",
+                          name: "odt_potion", url: "https://hex.pmhttps://hex.pm/packages/odt_potion", version: "0.0.1"}]
 
   test "fetch_xml" do
     response = HexSearcher.fetch_xml("cow")
